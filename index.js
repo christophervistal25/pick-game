0ce971cc07aa425c9ea50bce08fe85194a3ee998
unknown <=>
2019-03-17 16:01:17 +0800

    With Handling the Data'

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-let randomNumbers   = [];
-let positions       = [];
-let filledPositions = [];
+let parentElement = document.querySelector('#container');
+
+let noOfClick             = 0;
+let clickedItemValues     = [];
+let randomNumbers         = [];
+let positions             = [];
+let filledPositions       = [];
+const SECONDS_TO_DISPLAY  = 3000;
+const AFTER_CLICK_DISPLAY = 200;
 
 let boxes = [
-	[0,1,2,3],
-	[4,5,6,7],
-	[8,9,10,11],
-	[12,13,14,15]
+	[null , null , null , null],
+	[null , null , null , null],
+	[null , null , null , null],
+	[null , null , null , null]
 ];
 
 let isImageSame = (firstPick,secondPick) => firstPick === secondPick;
@@ -72,5 +78,78 @@ filledPositions.map((data,index) => {
 	}
 	boxes[arr[0]][arr[1]] = generatedPairs[index];
 });
-console.log(boxes);
 
+
+boxes.map((data , index) => {
+	data.map((values , keys) => {
+		let newButton = document.createElement('button');
+		let buttonContent = document.createTextNode(values);
+		newButton.setAttribute('id',`item_${index}_${keys}`);
+		newButton.setAttribute('onclick',`clickNumber(${[index,keys]})`);
+		newButton.style.padding = '15px';
+		newButton.appendChild(buttonContent);
+		parentElement.appendChild(newButton);
+	});
+});
+
+
+let clickNumber = (row,column) => {
+
+	document.querySelector(`#item_${row}_${column}`).setAttribute('disabled',true);
+	document.querySelector(`#item_${row}_${column}`).style.color='black';
+	document.querySelector(`#item_${row}_${column}`).innerHTML = boxes[row][column];
+
+	clickedItemValues.push(`${row},${column}`);
+	noOfClick++;
+	if (noOfClick % 2 == 0 && clickedItemValues.length == 2) {
+		let isCorrect = false;
+		let splitFirstPick   = clickedItemValues[0].split(',');
+		let splitSecondPick  = clickedItemValues[1].split(',');
+		let firstPickRow     = splitFirstPick[0];
+		let firstPickColumn  = splitFirstPick[1];
+		let secondPickRow    = splitSecondPick[0];
+		let secondPickColumn = splitSecondPick[1];
+
+		if (boxes[firstPickRow][firstPickColumn] == boxes[secondPickRow][secondPickColumn]) {
+			isCorrect = true;
+			console.log('Correct');
+		} else {
+			console.log('Wrong');
+		}
+
+		//rebase no of click
+		noOfClick = 0;
+
+		//rebase the holder for pair
+		clickedItemValues.length = 0;
+
+		//enabled the two pick of the user
+		if (isCorrect) { 
+			document.getElementById(`item_${firstPickRow}_${firstPickColumn}`)
+					.remove();
+			document.getElementById(`item_${secondPickRow}_${secondPickColumn}`)
+					.remove();
+		} else {
+			document.querySelector(`#item_${firstPickRow}_${firstPickColumn}`).removeAttribute('disabled');
+			document.querySelector(`#item_${secondPickRow}_${secondPickColumn}`).removeAttribute('disabled');	
+		}
+
+		delayTransparency(AFTER_CLICK_DISPLAY);
+
+	}
+};
+const transparentAllButtons = () => {
+	elements = document.getElementsByTagName('button');
+	for(let item of elements) {
+		item.innerHTML = ' ';
+		item.style.padding = '15px'
+	}
+};
+
+const delayTransparency = (SECONDS) => {
+	setTimeout(() => {
+		transparentAllButtons();
+	},SECONDS);
+};
+
+delayTransparency(SECONDS_TO_DISPLAY);
